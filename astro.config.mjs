db190ade0c9e818b1635303ad27088f63e2c0860
David Gasquez <davidgasquez@gmail.com>
2024-07-02 17:14:14 +0200

    chore: 🗑️ remove unused config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,4 @@ export default defineConfig({
   site: "https://davidgasquez.github.io",
   base: "/astro-static-dashboard",
   integrations: [vue()],
-  vite: {
-    ssr: {
-      external: ["nock", "mock-aws-s3", "aws-sdk"],
-    },
-  },
 });
